ccca4d8de60fe0b48be87443214af3e8c7806201
Jeroen <x>
2025-04-05 12:42:46 +0200

    Prep for preprocessing refactor

--- core/flg_test.py
+++ core/flg_test.py
@@ -37,6 +37,36 @@ def test_unet(update_reference=False):
         fls.dill_save(ref_name, np.std(heatmap))
     else:
         assert np.std(heatmap) == fls.dill_load(ref_name)
+
+def test_unet_alt(update_reference=False):
+    train_data = fls.load_all_train_data()
+    model = flg_unet.UNetModel()
+    model.deterministic_train = True
+    model.n_images_per_update = 6
+    model.dataset.size = (64,64,64)
+    model.dataset.offset_range_for_pos = (6,6,6)
+    model.n_images_test = 1
+    model.n_epochs = 2
+    model.seed = 42
+    model.dataset.normalize = 2
+    model2 = model
+    model = flg_model.ThreeStepModel()
+    model.step1Heatmap = model2
+    model.target_size = 320
+    model.train(train_data[1:3], train_data[4:5])
+    d = fls.load_all_test_data()[1]
+    d.load_to_memory()
+    heatmap = model.step1Heatmap.infer(d)
+    plt.figure()
+    plt.imshow(np.max(heatmap, axis=0), cmap='bone')
+    plt.colorbar()
+    plt.pause(0.01)
+
+    ref_name = fls.code_dir + 'ref_unet_alt.pickle';
+    if update_reference:
+        fls.dill_save(ref_name, np.std(heatmap))
+    else:
+        assert np.std(heatmap) == fls.dill_load(ref_name)
     
 
 def test_yolo(update_reference=False):
@@ -59,6 +89,7 @@ def test_yolo(update_reference=False):
 
 def run_all_tests(update_reference=False):
     test_unet(update_reference=update_reference)
+    test_unet_alt(update_reference=update_reference)
     test_yolo(update_reference=update_reference)
     print('All tests passed')
     
